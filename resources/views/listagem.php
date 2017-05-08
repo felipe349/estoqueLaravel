25f7dea32e6c9840984b88f6fa415adfb1e4a9f5
Felipe <claiohm@gmail.com>
2017-05-08 18:12:39 +0000

    Font Awesomes

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -1,16 +1,33 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <title>Controle de Estoque</title>
+    </head>
     <body>
-        <h1>Listagem de Produtos</h1>
-        <table>
-            <?php foreach($produtos as $p): ?>
-            <tr>
-                <td><?= $p->nome ?></td>
-                <td><?= $p->valor ?></td>
-                <td><?= $p->descricao ?></td>
-                <td><?= $p->quantidade ?></td>
-            </tr>
-            <?php endforeach ?>
-        </table>
+        <div class="container">
+            <h1>Listagem de Produtos</h1>
+            <table class="table table-striped table-bordered table-hover">
+                <?php foreach($produtos as $p): ?>
+                <tr>
+                    <td><?= $p->nome ?></td>
+                    <td><?= $p->valor ?></td>
+                    <td><?= $p->descricao ?></td>
+                    <td><?= $p->quantidade ?></td>
+                    <td><center><i class="fa fa-search fa-2x" style="color:#435AA6" aria-hidden="true"></center></i></td>
+                </tr>
+                <?php endforeach ?>
+            </table>
+        </div>
+        
+        
+        
+        <script
+            src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous">
+        </script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 </html>
